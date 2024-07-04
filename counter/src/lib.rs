3a21911bae5f7eef55dcd8ccdde41a8d28b03dbe
btwiuse <54848194+btwiuse@users.noreply.github.com>
2024-07-04 11:39:10 +0000

    add some comments

--- counter/src/lib.rs
+++ counter/src/lib.rs
@@ -5,22 +5,27 @@ use gstd::msg;
 static mut COUNTER: i32 = 0;
 
 #[no_mangle]
+unsafe extern fn init() {
+    gstd::debug!("whoami: {:?}", gstd::exec::program_id()); // 合约地址
+}
+
+#[no_mangle]
 unsafe extern fn handle() {
-    gstd::debug!("whoami: {:?}", gstd::exec::program_id());
-    gstd::debug!("sender: {:?}", msg::source());
+    gstd::debug!("sender: {:?}", msg::source()); // 用户地址
 
     let command = msg::load_bytes().expect("Invalid message");
 
-    gstd::debug!("command: {}", gstd::String::from_utf8_lossy(&command));
-    gstd::debug!("payload: {:x?}", &command);
+    gstd::debug!("payload: {:x?}", &command); // 消息 bytes
+    gstd::debug!("command: {}", gstd::String::from_utf8_lossy(&command)); // 消息 str
 
     match command.as_slice() {
-        b"inc" => COUNTER += 1,
-        b"dec" => COUNTER -= 1,
-        b"get" => (),
+        b"inc" /* 0x696e63 */ => COUNTER += 1,
+        b"dec" /* 0x646563 */ => COUNTER -= 1,
+        b"get" /* 0x676574 */ => (),
         _ => todo!(),
     };
 
+    gstd::debug!("counter: {}", COUNTER); // 更新后的状态
     msg::reply(COUNTER, 0).expect("Unable to reply");
 }
 
@@ -30,11 +35,15 @@ mod tests {
 
     #[test]
     fn my_counter_test() {
+        // create test environment
         let system = System::new();
         system.init_logger();
+
+        // create program instance
         let program = Program::current(&system);
         assert_eq!(program.id(), 1.into());
 
+        // initialize program
         let init_result = program.send_bytes(42, &[]);
         assert!(!init_result.main_failed());
 
